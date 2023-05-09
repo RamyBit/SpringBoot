4092bd1bc7481cc0f06f0fff320312975dd5157d
RamyBit <118202722+RamyBit@users.noreply.github.com>
2023-05-09 15:39:23 +0200

    BootApp added

---
+++ bootapp/src/main/java/com/example/bootapp/web/RestExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.bootapp.web;
+
+import com.example.bootapp.web.exception.BookNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    public RestExceptionHandler(){
+        super();
+    }
+    @ExceptionHandler(BookNotFoundException.class)
+    protected ResponseEntity<Object> handleNotFound(
+            Exception ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getLocalizedMessage(),new HttpHeaders(), HttpStatus.BAD_REQUEST,
+                request);
+    }
+}
